beabe53f1a913271a87c077587c5dfdde783fc68
WastonGura <WastonGura@outlook.com>
2026-05-14 02:57:41 +0800

    feat: finish the project structure

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+const Index = () => {
+  return (
+    <View className='index-container'>
+      <Text>Hello World!</Text>
+    </View>
+  )
+}
+
+export default Index
